9515fef0a136cbb99ff91f081ed3496a26ac21bf
AnabeGobel <anabegobel@gmail.com>
2026-09-20 22:03:18 +0100

    mudança do icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,21 +17,9 @@ export const metadata: Metadata = {
   description: 'Sistema administrativo de gestão de residências',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/sircah-favicon.svg',
+    shortcut: '/sircah-favicon.svg',
+    apple: '/sircah-favicon.svg',
   },
 }
 
